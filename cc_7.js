b3e1c1a4007a9106e508f9416e37d4711f6f21f9
tbingle <sandmanthomas9@icloud.com>
2025-02-15 16:32:12 -0500

    Task 1 - Customer Invoice Calculation

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,7 @@
 //Inital commit
+function calculateInvoice (subtotal, taxRate, discount) {
+    total = (subtotal + (subtotal * taxRate)) - discount
+    return `Total Invoice: $${total.toFixed(2)}`
+}
+console.log(calculateInvoice (100,.08,5))
+console.log(calculateInvoice (500,.1,20))
