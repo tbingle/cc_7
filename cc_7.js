0c85dfbd55a1e0de4fa774ad54943aee9bba3030
tbingle <sandmanthomas9@icloud.com>
2025-02-15 16:51:22 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -1,7 +1,16 @@
 //Inital commit
+//Task 1- Customer Invoice 
 function calculateInvoice (subtotal, taxRate, discount) {
     total = (subtotal + (subtotal * taxRate)) - discount
     return `Total Invoice: $${total.toFixed(2)}`
 }
-console.log(calculateInvoice (100,.08,5))
-console.log(calculateInvoice (500,.1,20))
+console.log(calculateInvoice (100,.08,5));//Log in console 
+console.log(calculateInvoice (500,.1,20));//Log
+
+//Task 2- Employee hourly wage 
+function calculateHourlyWage(salary, hoursPerWeek) {
+    hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage ${hourlyWage.toFixed(2)}`;
+}
+console.log(calculateHourlyWage(52000, 40))
+console.log(calculateHourlyWage(75000,35))
